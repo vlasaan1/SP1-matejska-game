0441295fc5d1c4548833be51a8ebb8472b12632e
Šimon Bouma <boumasim@fit.cvut.cz>
2024-03-05 22:39:34 +0100

    spawning tiles

---
+++ matejskavoblacich/Assets/Scripts/Pipes/PlayerGrid.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerGrid : MonoBehaviour
+{
+
+    [SerializeField] int size;
+
+    [SerializeField] Tile tilePrefab;
+
+    private Dictionary<Vector2, Tile> grid;
+
+    void Start(){
+        GenerateGrid();
+    }
+
+    void GenerateGrid(){
+        grid = new Dictionary<Vector2, Tile>();
+        for(int y = 0; y < size; y++){
+            for(int x = 0; x < size; x++){
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(y, x, 0), Quaternion.identity, transform);
+                spawnedTile.name = "Tile_" + y + "_" + x;
+
+                grid[new Vector2(y,x)] = spawnedTile;
+            }
+        }
+    }
+
+    public Tile GetTileAtPosition(Vector2 pos){
+        if(grid.TryGetValue(pos, out var tile)){
+            return tile;
+        }
+        return null;
+    }
+}
--- matejskavoblacich/Assets/Scripts/Pipes/Tile.cs
+++ matejskavoblacich/Assets/Scripts/Pipes/Tile.cs
@@ -2,22 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameField : MonoBehaviour
+public class Tile : MonoBehaviour
 {
-
-    int size;
-
-    int bombs;
-    [SerializeField] int numberOfBombs;
-
-    [SerializeField] SingePipe pipePrefab;
-
-    SingePipe [,] playingField;
-
-    void InicializeGameField(int size, int bombs){
-
-    }
-
     // Start is called before the first frame update
     void Start()
     {
