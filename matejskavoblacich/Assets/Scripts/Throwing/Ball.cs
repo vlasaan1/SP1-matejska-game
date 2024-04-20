f339c7ccc637d16127c99e259724078651cde740
Jirka <bryndji3@cvut.cz>
2024-04-20 10:05:27 +0200

    Added ball hit sound

--- matejskavoblacich/Assets/Scripts/Throwing/Ball.cs
+++ matejskavoblacich/Assets/Scripts/Throwing/Ball.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    AudioSource audioSource;
     public bool isInQueue = false;
+
+    void Start (){
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void OnCollisionEnter2D(Collision2D coll){
+        //Used later for playing sound
+        //Debug.Log(coll.relativeVelocity.magnitude);
+        audioSource.volume = Mathf.Clamp(coll.relativeVelocity.magnitude/10,0,1);
+        audioSource.Play();
+    }
 }
