0ff214d3a808986e6adcbdabcb69becb9b67514f
Jirka <bryndji3@cvut.cz>
2024-04-20 11:36:48 +0200

    Bonus Ball

--- matejskavoblacich/Assets/Scripts/Throwing/Ball.cs
+++ matejskavoblacich/Assets/Scripts/Throwing/Ball.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     AudioSource audioSource;
+    SpriteRenderer spriteRenderer;
     public bool isInQueue = false;
+    bool bonusBall = false;
 
     void Start (){
         audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
     public void OnCollisionEnter2D(Collision2D coll){
-        //Used later for playing sound
-        //Debug.Log(coll.relativeVelocity.magnitude);
-        audioSource.volume = Mathf.Clamp(coll.relativeVelocity.magnitude/10,0,1);
+        audioSource.volume = Mathf.Clamp(coll.relativeVelocity.magnitude/15,0,1);
         audioSource.Play();
+    }
+
+    public void TurnOnBonus(){
+        bonusBall = true;
+        spriteRenderer.material.SetInt("_EnableBonus",1);
+    }
+
+    public void TurnOffBonus(){
+        if(bonusBall){
+            bonusBall = false;
+            spriteRenderer.material.SetInt("_EnableBonus",0);
+        }
+    }
+
+    public bool IsBonus(){
+        return bonusBall;
     }
 }
