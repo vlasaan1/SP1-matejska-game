9e4ba37cade2dcdd3736cb88196f0721c44b8674
Jirka <bryndji3@cvut.cz>
2024-05-04 14:28:36 +0200

    sounds are working

--- matejskavoblacich/Assets/Scripts/Throwing/Ball.cs
+++ matejskavoblacich/Assets/Scripts/Throwing/Ball.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Ball : BaseHittable
 {
+    [SerializeField] List<AudioClip> ballSoundClips;
     AudioSource audioSource;
     SpriteRenderer spriteRenderer;
     public bool isInQueue = false;
@@ -16,6 +17,7 @@ public class Ball : BaseHittable
     }
     public void OnCollisionEnter2D(Collision2D coll){
         audioSource.volume = Mathf.Clamp(coll.relativeVelocity.magnitude/15,0,1);
+        audioSource.clip = ballSoundClips[Random.Range(0,ballSoundClips.Count)];
         audioSource.Play();
     }
 
